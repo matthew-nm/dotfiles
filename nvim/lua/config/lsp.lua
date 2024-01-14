3bf97899b1b9e2051c2e0c00a01762dd970555fa
Matt M <matthewmalek@gmail.com>
2024-01-13 21:23:43 -0800

    nvim: Code hints in floating windows, not inline

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -6,10 +6,31 @@ local lsp = require('lsp-zero').preset({
 })
 
 lsp.on_attach(function(client, bufnr)
+
   lsp.default_keymaps({buffer = bufnr})
+
+  vim.api.nvim_create_autocmd("CursorHold", { -- show code hints in floating window
+    buffer = bufnr,
+    callback = function()
+      local opts = {
+        focusable = false,
+        close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+        border = 'rounded',
+        source = 'always',
+        prefix = ' ',
+        scope = 'cursor',
+      }
+      vim.diagnostic.open_float(nil, opts)
+    end
+  })
 end)
 
 lsp.setup()
+
+
+vim.diagnostic.config({ -- hide inline code hints
+	virtual_text = false,
+})
 
 
 ----- Configure CMP -----
