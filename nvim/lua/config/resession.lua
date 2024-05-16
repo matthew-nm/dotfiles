e7c21cd8638f8f459b300824d5579a520ee6edac
Matt M <matthewmalek@gmail.com>
2024-05-15 19:46:16 -0700

    nvim: Update resession keybinds

--- nvim/lua/config/resession.lua
+++ nvim/lua/config/resession.lua
@@ -2,6 +2,6 @@ local resession = require('resession')
 
 resession.setup()
 
-vim.keymap.set('n', '<leader>ss', resession.save)
-vim.keymap.set('n', '<leader>sl', resession.load)
-vim.keymap.set('n', '<leader>sd', resession.delete)
+vim.keymap.set('n', '<leader>Ss', resession.save)
+vim.keymap.set('n', '<leader>Sl', resession.load)
+vim.keymap.set('n', '<leader>Sd', resession.delete)
