a900c91345bba0aa229c1ece977358a25ea1ec4f
Matthew Malekhedayat <Matthew.Malekhedayat@ucsf.edu>
2024-08-12 13:09:02 -0700

    nvim: Add border to autocomplete popup

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -77,7 +77,7 @@ cmp.setup({
     end,
   },
   window = {
-    -- completion = cmp.config.window.bordered(),
+    completion = cmp.config.window.bordered(),
     documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
@@ -96,4 +96,4 @@ cmp.setup({
     { name = 'buffer' },
     { name = 'path' },
   },
-  })
+})
