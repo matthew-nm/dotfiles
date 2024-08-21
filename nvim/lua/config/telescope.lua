41a980139fdc9220f8ce1f67cf4ef33be9c73695
Matt M <matthewmalek@gmail.com>
2024-08-21 15:19:37 -0700

    nvim: Format

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -3,28 +3,36 @@ require('telescope').load_extension('fzf') -- enable fzf
 require('yanky').setup() -- init yanky
 require('telescope').load_extension('yank_history') -- enable yanky
 
-require('telescope').setup({extensions = { -- configure undo
-  undo = {
-    side_by_side = true,
-    layout_strategy = 'vertical',
-    layout_config = {preview_height = 0.7},
-    vim_diff_opts = {ctxlen = 0},
-    entry_format = 'state #$ID, $STAT, $TIME',
-    time_format = '',
-    mappings = {
-      i = {
-        ['<CR>'] = require('telescope-undo.actions').restore,
-        ['<C-a>'] = require('telescope-undo.actions').yank_additions,
-        ['<C-d>'] = require('telescope-undo.actions').yank_deletions,
+-- configure undo
+require('telescope').setup({
+  extensions = {
+    undo = {
+      side_by_side = true,
+      layout_strategy = 'vertical',
+      layout_config = { preview_height = 0.7 },
+      vim_diff_opts = { ctxlen = 0 },
+      entry_format = 'state #$ID, $STAT, $TIME',
+      time_format = '',
+      mappings = {
+        i = {
+          ['<CR>'] = require('telescope-undo.actions').restore,
+          ['<C-a>'] = require('telescope-undo.actions').yank_additions,
+          ['<C-d>'] = require('telescope-undo.actions').yank_deletions,
+        },
       },
     },
-  },
-}})
-require('telescope').load_extension('undo') -- enable undo history
+  }
+})
+require('telescope').load_extension('undo')
 
-require('telescope').setup({defaults = {preview = { -- configure previewer
-  check_mime_type = true,
-  filesize_limit = 8,
-  timeout = 250,
-  treesitter = true,
-}}})
+-- configure preview
+require('telescope').setup({
+  defaults = {
+    preview = {
+      check_mime_type = true,
+      filesize_limit = 8,
+      timeout = 250,
+      treesitter = true,
+    }
+  }
+})
